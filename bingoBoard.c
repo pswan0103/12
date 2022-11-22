e95ba48cea724875796bb6a7ba03c6c1ccb2be29
pswan0103 <22zch@naver.com>
2022-11-22 18:43:50 +0900

    4~6

--- bingoBoard.c
+++ bingoBoard.c
@@ -8,26 +8,47 @@
 static int bingoBoard[N_SIZE][N_SIZE];
 static int numberStatus [N_SIZE*N_SIZE];
 
+
+int bingo_checkNum(int selNum)
+{
+	if (numberStatus[selNum-1] == BINGONUM_HOLE)
+		return BINGO_NUMSTATUS_ABSENT;
+
+	else
+		return BINGO_NUMSTATUS_PRESENT;
+	
+}
+
 void bingo_init(void) 
 {
-	int i, j;
-	int cnt=1;
+	int i, j, k;
+	int randNum;
+	int maxNum = N_SIZE*N_SIZE;
 	
-	for (i=0;i<N_SIZE;i++)
-		for (j=0;j<N_SIZE;j++)
-		{
-			if (cnt == 15)
-			{
-				bingoBoard[i][j]=BINGONUM_HOLE;
-				numberStatus[cnt-1]=BINGONUM_HOLE;
-				cnt++;
+	for (i=0; i<N_SIZE*N_SIZE; i++)
+		numberStatus[i] = -1;
+		
+	for (i=0; i<N_SIZE; i++) {
+		for (j=0; j<N_SIZE; j++) {
+			randNum = rand()%(maxNum-1);
+			
+			for (k=0; k<N_SIZE*N_SIZE; k++) {
+				if (numberStatus[k] == BINGO_NUMSTATUS_ABSENT) //할당 안 됨 
+				{
+					if (randNum == 0) {
+						break;
+					} 
+					else
+					{
+						randNum--;
+					}
+				}
 			}
-			else
-			{
-				numberStatus[cnt-1] = i*N_SIZE + j;
-				bingoBoard[i][j]=cnt++;
-			}
+			bingoBoard[i][j] = k+1; 
+			numberStatus[k] = N_SIZE*i+j;
+			//maxNum--;
 		}
+	}
 }
 
 void bingo_printBoard(void) 
@@ -55,9 +76,72 @@ void bingo_inputNum(int sel)
 	index_C = numberStatus[sel-1]%N_SIZE;
 	
 	bingoBoard[index_R][index_C] = BINGONUM_HOLE;
+	numberStatus[sel-1] = BINGO_NUMSTATUS_ABSENT;
 }
 
 int bingo_countCompletedLine(void) 
 {
+	int i, j;
+	int cnt=0;
+	int checkBingo;
 	
+	
+	//행 
+	for (i=0; i<N_SIZE; i++) {
+		checkBingo = 1; 
+		for (j=0; j<N_SIZE; j++){
+			if (bingoBoard[i][j] > 0) {
+				checkBingo=0;
+				break;
+			}
+		}
+		
+		if (checkBingo == 1) {
+			cnt++;
+		}
+	}
+	
+	//열 
+	for (j=0; j<N_SIZE; j++) {
+		checkBingo = 1;
+		for (i=0; i<N_SIZE; i++) {
+			if (bingoBoard[i][j] > 0) {
+				checkBingo=0;
+				break;
+			}
+		}
+		if (checkBingo == 1){
+			cnt++;
+		}
+	}
+	
+	//대각선 1
+	for (i=0; i<N_SIZE; i++) {
+		checkBingo = 1;
+			if (bingoBoard[i][i] > 0) {
+				checkBingo=0;
+				break;
+			}
+		}
+		
+		if (checkBingo == 1)
+			cnt++;
+	
+	//대각선 2 
+	for (i=0; i<N_SIZE; i++) {
+		checkBingo = 1; 
+		if (bingoBoard[i][N_SIZE-i-1] > 0) {
+			checkBingo=0;
+			break;
+		}
+	}
+		if (checkBingo == 1)
+			cnt++;
+	
+	return cnt;
 }
+
+
+
+
+
--- main.c
+++ main.c
@@ -3,11 +3,46 @@
 #include <time.h>
 #include "bingoBoard.h"
 
+#define BINGO_RES_UNFINISHED	-1
+#define BINGO_RES_FINISHED	0
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int check_gameEnd(void)	
+{
+	
+	int res = BINGO_RES_UNFINISHED;
+	
+	if (bingo_countCompletedLine() >= N_BINGO )
+		res = BINGO_RES_FINISHED;
+	
+	return res;
+}
+
+int get_number(void) 
+{
+	int selNum=0;
+	
+	do {
+		printf("select a number: ");
+		scanf("%d", &selNum);
+		fflush(stdin);
+		
+		if (selNum < 1 || selNum > N_SIZE*N_SIZE || bingo_checkNum(selNum) == BINGO_NUMSTATUS_ABSENT)
+		{
+			printf("%i is not on the board! select other one.\n", selNum);
+		}
+		
+	} while(selNum < 1 || selNum > N_SIZE*N_SIZE || bingo_checkNum(selNum) == BINGO_NUMSTATUS_ABSENT);
+	
+	return selNum;
+}
 
 int main(int argc, char *argv[]) {
 	
 	srand((unsigned)time(NULL));
+	
+	int selNum;
 	
 	//opening
 	printf("==================================\n");
@@ -16,23 +51,26 @@ int main(int argc, char *argv[]) {
 	
 	
 	//game
+//	bingo_init();
+//	bingo_printBoard();
+//	selNum = get_number();
+//	bingo_inputNum(selNum);
+//	bingo_printBoard();
+	
 	bingo_init();
-	bingo_printBoard();
-	bingo_inputNum(5);
-	bingo_printBoard();
 	
-	//initialize boards
-	
-	//while (gamr id not end)
-	/*
+	while (check_gameEnd() == BINGO_RES_UNFINISHED)
 	{
-		//print board
-		//print completed lines
-		//select a number
-		//put the number on the board
+		bingo_printBoard();
+		
+		selNum=get_number();
+		
+		bingo_inputNum(selNum);
+		
+		printf("No. of completed line is %i\n", bingo_countCompletedLine());
 	}
-	*/
 	
+	bingo_printBoard();
 	
 	//ending
 	printf("\n\n\n\n\n\n\n\n\n\n");
@@ -45,3 +83,6 @@ int main(int argc, char *argv[]) {
 	
 	return 0;
 }
+
+
+
